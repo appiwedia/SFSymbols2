60fc5fd9505137f5c14c097cb931dbd39a7d6c64
Mickael MAS <mickael.mas@appiwedia.com>
2020-07-17 14:12:11 +0200

    Initial Commit

---
+++ SFSymbols2/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SFSymbols2
+//
+//  Created by Mickael Mas on 17/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SFSymbols2/SFSymbols2App.swift
@@ -0,0 +1,17 @@
+//
+//  SFSymbols2App.swift
+//  SFSymbols2
+//
+//  Created by Mickael Mas on 17/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct SFSymbols2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
